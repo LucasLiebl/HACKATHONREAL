8d82bd816a59febc6e56c8ffef056a45c2be1c69
eduardo monta <emontandon11@gmail.com>
2023-07-28 16:42:05 -0300

    Login Input att

---
+++ src/components/InputType.vue
@@ -0,0 +1,37 @@
+<script setup>
+const props = defineProps({
+  placeholder: String,
+  vmodel: String
+})
+
+</script>
+
+<template>
+
+<input class="input" :v-model="props.vmodel" :placeholder="props.placeholder">
+
+</template>
+
+<style scoped>
+.input{
+    border: none;
+  outline: none;
+    width: 508px;
+    height: 68px;
+    border-radius: 10px;
+background: #F1F1F1;
+box-shadow: 4px 4px 4px 0px rgba(54, 53, 53, 0.25);
+
+}
+.input::placeholder{
+  color: #8F8E8E;
+font-family: Nunito;
+font-size: 20px;
+font-style: normal;
+font-weight: 900;
+line-height: normal;
+text-transform: uppercase;
+padding-left: 20px;
+transform: translate(0, 10%);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,14 @@
 <script setup>
+import InputType from '../components/InputType.vue'
+const placeholders = ['texto 1', 'texto2', 'kjfkdjkjdk  ']
+const vmodel = ['texto dsds', 'texto2dds', 'dsdsds  ']
 </script>
-
 <template>
-  <main>
-  </main>
+
+  <input-type v-for="(placeholder, i) in placeholders" :key="i" :placeholder="placeholder" :vmodel="vmodel" />
+
 </template>
+
+<style>
+
+</style>
