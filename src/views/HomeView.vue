3e41f0a21c3d76aaec50c1b8afa475a67365cbf5
= <=>
2023-08-03 14:01:27 -0300

    Merge to Master

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,8 @@
 <script setup>
-import InputType from '../components/login components/InputType.vue'
-import ButtonType from '../components/login components/ButtonType.vue';
-const placeholder = 'texto 1'
-const type = 'email';
-const icon = 'envelope'
+
 </script>
 <template>
 
-  <input-type :type="type" :icon="icon" :placeholder="placeholder" />
-
-  <button-type buttontext="teste"/>
 </template>
 
 <style>
