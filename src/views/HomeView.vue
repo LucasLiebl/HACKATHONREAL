7c25cee7cc4308a3bdaa08c576e444c543591946
= <=>
2023-08-03 14:00:28 -0300

    Button finished

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import InputType from '../components/login components/InputType.vue'
+import ButtonType from '../components/login components/ButtonType.vue';
 const placeholder = 'texto 1'
 const type = 'email';
 const icon = 'envelope'
@@ -8,6 +9,7 @@ const icon = 'envelope'
 
   <input-type :type="type" :icon="icon" :placeholder="placeholder" />
 
+  <button-type buttontext="teste"/>
 </template>
 
 <style>
