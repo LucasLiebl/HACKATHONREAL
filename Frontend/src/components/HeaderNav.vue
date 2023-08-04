e3254dd61cb92878f5c7a63d6f8c1a7f87333cc6
Eduardo Monta <emontandon11@gmail.com>
2023-08-04 16:47:22 -0300

    Header att

---
+++ Frontend/src/components/HeaderNav.vue
@@ -0,0 +1,64 @@
+<script setup>
+const props = defineProps({
+    text1: String,
+    text2: String,
+})
+
+</script>
+<template>
+    <header class="header">
+        <div class="logo"><img src="https://i.imgur.com/7TZ6Sm6.png" alt="">
+            <h1>Jame's</h1>
+        </div>
+        <nav class="NavText">
+            <h1>teste</h1>
+            <h1>teste</h1>
+        </nav>
+    </header>
+</template>
+
+<style>
+.header {
+    display: flex;
+    width: 100%;
+    height: 100px;
+    background: linear-gradient(90deg, #FF4848 0%, #FF6E4F 96.57%);
+    padding-left: 20px;
+    align-items: center;
+    justify-content: space-between;
+}
+
+img {
+    width: 70px;
+    height: 70px;
+}
+
+h1 {
+    color: #FFF;
+    font-family: Nunito;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 900;
+    line-height: normal;
+
+}
+
+.logo {
+    display: grid;
+    justify-items: center;
+}
+
+.NavText {
+    display: grid;
+    grid-template-rows: 1fr;
+    grid-template-columns: 1fr 1fr;
+}
+
+.NavText h1 {
+    color: #FFF;
+    font-family: Nunito;
+    font-size: 48px;
+    font-style: normal;
+    font-weight: 900;
+    line-height: normal;
+}</style>
---
+++ Frontend/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HeaderNav from '../components/HeaderNav.vue';
+import ButtonType from '../components/login/ButtonType.vue';
+import InputType from '../components/login/InputType.vue'
+
+
+</script>
+<template>
+<header-nav/>
+<input-type placeholder="eaea" icon="envelope"/>
+<input-type placeholder="fdd" icon="envelope"/>
+
+<button-type/>
+</template>
+
+<style>
+
+</style>
