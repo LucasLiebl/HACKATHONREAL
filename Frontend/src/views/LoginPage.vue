7ce21dfc0917cea020574759bb38e15a57db35e0
Eduardo Monta <emontandon11@gmail.com>
2023-08-08 11:56:02 -0300

    LoginPage Atts

---
+++ Frontend/src/views/LoginPage.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HeaderNav from '../components/HeaderNav.vue'
+import ButtonType from '../components/login/ButtonType.vue'
+import ButtonType from '../components/login/ButtonType.vue'
+
+
+</script>
+<template>
+    
+
+</template>
+
+<style>
+
+svg{
+}
+
+</style>
